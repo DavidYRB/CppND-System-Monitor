c278a0ffef166d3fd8a59c3165e16939d4803961
ruibo <david.yanrb@gmail.com>
2020-03-03 12:40:29 -0500

    * Use priority queue to sort process based on cpu usage

--- src/system.cpp
+++ src/system.cpp
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <cstddef>
+#include <queue>
 #include <set>
 #include <string>
 #include <vector>
@@ -25,42 +26,23 @@ Processor& System::Cpu() { return cpu_; }
 
 // Return a container composed of the system's processes
 vector<Process>& System::Processes() {
-    //std::cout <<"startd getting process\n";
     if(processes_.size() != 0){
         processes_.erase(processes_.begin(), processes_.end());
     }
-    //std::cout <<"Get all pids\n";
     vector<int> pids = LinuxParser::Pids();
-    //std::cout <<"Find uid user map\n";
     std::unordered_map<std::string, std::string> uidUserMap;
     LinuxParser::FindUidUser(uidUserMap);
-    //std::cout <<"uidUserMap size: " << uidUserMap.size() << '\n';
+    auto cmp = [](Process& a, Process& b) {return a < b;};
+    std::priority_queue<Process, vector<Process>, decltype(cmp)> process_queue(cmp);
     vector<int>::iterator pidIt;
-    //std::cout <<"Start to put processes in order\n";
     for(pidIt = pids.begin(); pidIt != pids.end(); pidIt++){
-        //std::cout <<"PID: " << *pidIt << '\n';
         Process processTemp(*pidIt, uidUserMap);
-        //std::cout <<"Created temp process object\n";
-        processTemp.CpuTimePrev(Cpu().TotalTime());
-        if(processes_.empty()){
-            //std::cout <<"vector is empty\n";
-            processes_.push_back(processTemp);
-            continue;
-        }
-        else if(*(processes_.begin()) < processTemp){
-            processes_.insert(processes_.begin(), processTemp);
-            //std::cout <<"Insert at head\n";
-            continue;
-        }
-        for(auto processIt = processes_.begin(); processIt != processes_.end(); processIt++){
-            if(processTemp < *processIt){
-                if(*(processIt + 1) < processTemp || processIt + 1 == processes_.end()){
-                    processes_.insert(processIt + 1, processTemp);
-                }
-            }
-        }
+        process_queue.push(processTemp);
     }
-    std::cout <<"Finished process parsing\n";
+    while(!process_queue.empty()){
+        processes_.push_back(process_queue.top());
+        process_queue.pop();
+    }
     return processes_;
 }
 
